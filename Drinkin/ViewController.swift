c5cd49586df9ae0c6a015530b1fffb3cae0db386
kimseongj <ksj8277@naver.com>
2023-04-03 23:40:45 +0900

    initial commit with removing StoryBoard and making gitingnore

---
+++ Drinkin/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/04/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .brown
+    }
+}
+
