97ad7ccc5549c1c99c1c9d96e6dd78d4fd813081
kimseongj <ksj8277@naver.com>
2023-04-16 15:37:33 +0900

    feat: RefreshEndpoint 구현

---
+++ Drinkin/Network/RefreshEndpoint.swift
@@ -0,0 +1,24 @@
+//
+//  RefreshEndpoint.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/04/16.
+//
+
+import Foundation
+
+struct RefreshEndpoint: EndpointMakeable {
+    var baseURL: String = "http://3.35.208.50"
+    
+    var path: String = "/v1/home"
+    
+    var method: String = HTTPMethod.get.rawValue
+    
+    var queryItems: [URLQueryItem] = []
+    
+    var header: [String : String]?
+    
+    mutating func insertAuthorization(key: String) {
+        header = ["Authorization": key]
+    }
+}
