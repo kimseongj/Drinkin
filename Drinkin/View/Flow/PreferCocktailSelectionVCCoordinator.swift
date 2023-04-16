b848901779f873478c6b9325d5f6b7a3dce434fd
kimseongj <ksj8277@naver.com>
2023-04-16 12:47:12 +0900

    feat: View 구현 및 Provider 구현

---
+++ Drinkin/View/Flow/PreferCocktailSelectionVCCoordinator.swift
@@ -0,0 +1,29 @@
+//
+//  PreferCocktailSelectionVCCoordinator.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/04/14.
+//
+
+import UIKit
+
+class PreferCocktailSelectionViewCoordinator: Coordinator {
+    weak var parentCoordinator: Coordinator?
+    var childCoordinators: [Coordinator] = []
+    var navigationController: UINavigationController
+
+    init(navigationController: UINavigationController) {
+        self.navigationController = navigationController
+    }
+
+    func start() {
+        let vc = PreferCocktailSelectionViewController()
+        vc.modalPresentationStyle = .fullScreen
+        //preferCocktailSelectionViewController.delegate = self
+        navigationController.present(vc, animated: true)
+    }
+    
+    //func push
+    
+
+}
--- Drinkin/View/ModalView/BuildModalViewController.swift
+++ Drinkin/View/ModalView/BuildModalViewController.swift
@@ -5,4 +5,27 @@
 //  Created by kimseongjun on 2023/04/06.
 //
 
-import Foundation
+import UIKit
+
+class BuildModalViewController: UIViewController {
+    
+    let imageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    let descriptionLabel: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .black
+    }
+}
