778d2624b529850c238d4ad6698733e6047ba98c
kimseongj <ksj8277@naver.com>
2023-07-28 15:43:14 +0900

    refactor: CocktailRecommendViewModel를 DI를 위해 프로토콜 구현

---
+++ Drinkin/Present/TabBar/Main/CocktailRecommendViewModel/CocktailRecommendViewModel.swift
@@ -0,0 +1,37 @@
+//
+//  LoggedinMainViewModel.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/07/22.
+//
+
+import Foundation
+import Combine
+
+protocol CocktailRecommendViewModel {
+    
+}
+
+class DefaultCocktailRecommendViewModel: CocktailRecommendViewModel {
+    @Published var resultList: [Result] = []
+    private var cancelBag: Set<AnyCancellable> = []
+    
+    private let fetchBriefDescriptionUseCase: FetchBriefDescriptionUsecase
+    
+    init(fetchBriefDescriptionUseCase: FetchBriefDescriptionUsecase) {
+        self.fetchBriefDescriptionUseCase = fetchBriefDescriptionUseCase
+        fetchBriefDescription()
+    }
+    
+    func fetchBriefDescription() {
+        fetchBriefDescriptionUseCase.execute().sink(receiveCompletion: { print("\($0)")}, receiveValue: {
+            self.resultList = $0.results
+            print($0) }).store(in: &cancelBag)
+    }
+    
+    func fetchResult(completion: @escaping () -> Void) {
+        $resultList.sink {
+            completion($0)
+        }
+    }
+}
