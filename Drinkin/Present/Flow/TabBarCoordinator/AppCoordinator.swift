5371cf7b7d29005aca350bb155e77090909806eb
kimseongj <ksj8277@naver.com>
2023-09-05 16:47:34 +0900

    refactor: TabBarController 삭제 및 AppCoordinator에 메서드로 TabBarController 구현

--- Drinkin/Present/Flow/TabBarCoordinator/AppCoordinator.swift
+++ Drinkin/Present/Flow/TabBarCoordinator/AppCoordinator.swift
@@ -19,7 +19,31 @@ class AppCoordinator: NSObject, Coordinator {
     
     func start() {
         let tabBarController = TabBarController()
-        self.window?.rootViewController = tabBarController
+        self.window?.rootViewController = configureTabBarController()
         AppCoordinator.tabBarHeight = tabBarController.tabBar.frame.size.height
+    }
+    
+    func configureTabBarController() -> UITabBarController {
+        let tabBarController = UITabBarController()
+        
+        let mainTabBarItem = UITabBarItem(title: "메인", image: UIImage(named: "home_icon"), selectedImage: UIImage(named: "home_fill_icon"))
+        let cocktailTabBarItem = UITabBarItem(title: "칵테일", image: UIImage(named: "cocktail_list_icon"), selectedImage: UIImage(named: "cocktail_list_fill_icon"))
+        let homeBarTabBarItem = UITabBarItem(title: "나의 홈바", image: UIImage(named: "my_bar_icon"), selectedImage: UIImage(named: "my_bar_fill_icon"))
+        
+        let mainVCCoordinator = MainVCCoordinator()
+        let mainViewController = mainVCCoordinator.startPush()
+        mainViewController.tabBarItem = mainTabBarItem
+        
+        let cocktailVCCoordinator = CocktailVCCoordinator()
+        let cocktailViewController = cocktailVCCoordinator.startPush()
+        cocktailViewController.tabBarItem = cocktailTabBarItem
+        
+        let homeBarVCCoordinator = HomeBarVCCoordinator()
+        let homeBarViewController = homeBarVCCoordinator.startPush()
+        homeBarViewController.tabBarItem = homeBarTabBarItem
+        
+        tabBarController.viewControllers = [mainViewController, cocktailViewController, homeBarViewController]
+        
+        return tabBarController
     }
 }
