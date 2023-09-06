f31013e7116ee859b1451eebe77c499512a9ef84
kimseongj <ksj8277@naver.com>
2023-09-06 15:50:56 +0900

    refactor: configureViewState메서드명을 renewCompleteSelectionButton으로 변경

--- Drinkin/Present/TriedCocktailSelection/TriedCocktialSelectionViewController.swift
+++ Drinkin/Present/TriedCocktailSelection/TriedCocktialSelectionViewController.swift
@@ -95,7 +95,7 @@ final class TriedCocktailSelectionViewController: UIViewController {
         MainViewController.login = true
         configureCocktailCollectionView()
         configureBaseTypeCollectionView()
-        configureViewState()
+        renewCompleteSelectionButton()
         configureCocktailDataSource()
         binding()
         viewModel?.fetchCocktailPreviewDescription()
@@ -174,10 +174,11 @@ final class TriedCocktailSelectionViewController: UIViewController {
         }
     }
     
-    private func configureViewState(isCellsSelected: Bool = false) {
+    private func renewCompleteSelectionButton(isCellsSelected: Bool = false) {
         switch isCellsSelected {
         case false:
             completeSelectionButton.setTitle("다음", for: .normal)
+            completeSelectionButton.removeTarget(self, action: #selector(presentLoginViewController), for: .touchUpInside)
             completeSelectionButton.addTarget(self, action: #selector(presentPopupViewController), for: .touchUpInside)
         case true:
             completeSelectionButton.setTitle("선택 완료", for: .normal)
@@ -240,7 +241,7 @@ extension TriedCocktailSelectionViewController: UICollectionViewDelegate {
                 cell.presentSelected()
             }
             viewModel?.selectCocktail(index: indexPath.row)
-            configureViewState(isCellsSelected: viewModel?.checkCocktailSelected() ?? false )
+            renewCompleteSelectionButton(isCellsSelected: viewModel?.checkCocktailSelected() ?? false )
         }
     }
     
@@ -249,7 +250,7 @@ extension TriedCocktailSelectionViewController: UICollectionViewDelegate {
             guard let cell = cocktailCollectionView.cellForItem(at: indexPath) as? CocktailSelectionCell else { return }
             
             viewModel?.deselectCocktail(index: indexPath.row)
-            configureViewState(isCellsSelected: viewModel?.checkCocktailSelected() ?? false )
+            renewCompleteSelectionButton(isCellsSelected: viewModel?.checkCocktailSelected() ?? false )
             cell.presentDeselected()
         }
     }
@@ -320,6 +321,7 @@ extension TriedCocktailSelectionViewController {
 //MARK: - Dismiss ViewController
 extension TriedCocktailSelectionViewController: DismissDelegate {
     func dismissCurrentViewController() {
-        self.dismiss(animated: true)
+        //self.dismiss(animated: true)
+        delegate?.presentLoginVC()
     }
 }
