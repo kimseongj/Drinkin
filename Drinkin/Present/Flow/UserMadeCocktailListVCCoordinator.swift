41ec6894a855f1b275f1b93ec2fd60559761bf13
kimseongj <ksj8277@naver.com>
2023-09-09 17:14:03 +0900

    feat: UserMadeCocktailListVCCoordinator 구현

--- Drinkin/Present/Flow/UserMadeCocktailListVCCoordinator.swift
+++ Drinkin/Present/Flow/UserMadeCocktailListVCCoordinator.swift
@@ -1,13 +1,13 @@
 //
-//  SavedCocktailListVCCoordinator.swift
+//  UserMadeCocktailListVCCoordinator.swift
 //  Drinkin
 //
-//  Created by kimseongjun on 2023/09/08.
+//  Created by kimseongjun on 2023/09/09.
 //
 
 import UIKit
 
-class SavedCocktailListVCCoordinator: Coordinator {
+class UserMadeCocktailListVCCoordinator: Coordinator {
     weak var parentCoordinator: Coordinator?
     var childCoordinators: [Coordinator] = []
     var navigationController: UINavigationController
@@ -17,7 +17,7 @@ class SavedCocktailListVCCoordinator: Coordinator {
     }
 
     func start() {
-        let vc = CocktailListViewController(viewControllerTitle: "저장한 칵테일 목록")
+        let vc = CocktailListViewController(viewControllerTitle: "만들어본 칵테일 목록")
         navigationController.pushViewController(vc, animated: true)
     }
     
