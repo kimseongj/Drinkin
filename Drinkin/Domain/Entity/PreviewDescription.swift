5381617d5e60bae7f3364a147fb772279b15d3ce
kimseongj <ksj8277@naver.com>
2023-09-11 18:11:56 +0900

    refactor: Entity 개편, 구조체명 변경

---
+++ Drinkin/Domain/Entity/PreviewDescription.swift
@@ -0,0 +1,32 @@
+//
+//  CocktailPreviewDescription.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/09/10.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct CocktailPreviewDescription: Codable {
+    let cocktailList: [PreviewDescription]
+
+    enum CodingKeys: String, CodingKey {
+        case cocktailList = "cocktail_list"
+    }
+}
+
+// MARK: - CocktailList
+struct PreviewDescription: Codable, Hashable {
+    let id = UUID()
+    let title: String
+    let levelGrade, sugarContentGrade, abvGrade, ingredientQuantity: Int
+
+    enum CodingKeys: String, CodingKey {
+        case title
+        case levelGrade = "level_grade"
+        case sugarContentGrade = "sugar_content"
+        case abvGrade = "abv_level"
+        case ingredientQuantity
+    }
+}
