efb16e42dd845ea581f2c592a8a85cf5366fe9fb
kimseongj <ksj8277@naver.com>
2023-09-17 20:41:53 +0900

    feat: CocktailListEndpoint 구현

---
+++ Drinkin/Network/Endpoint/UIEndpoint/CocktailListEndpoint.swift
@@ -0,0 +1,20 @@
+//
+//  CocktailListEndpoint.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/09/15.
+//
+
+import Foundation
+
+struct CocktailListEndpoint: EndpointMakeable {
+    var baseURL: String = "https://19fdd04c-1102-4b20-b18b-37ca653fa0da.mock.pstmn.io"
+    
+    var path: String = "/cocktaillist"
+    
+    var method: String = HTTPMethod.get.rawValue
+    
+    var queryItems: [URLQueryItem] = []
+    
+    var header: [String : String]?
+}
