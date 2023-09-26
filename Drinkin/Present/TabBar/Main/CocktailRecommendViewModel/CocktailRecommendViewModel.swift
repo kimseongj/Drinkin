cf174baa30b0b8738098cdcaeefacb6c08ff22a7
kimseongj <ksj8277@naver.com>
2023-09-26 12:01:56 +0900

    refactor: 프로퍼티 위치 변경

--- Drinkin/Present/TabBar/Main/CocktailRecommendViewModel/CocktailRecommendViewModel.swift
+++ Drinkin/Present/TabBar/Main/CocktailRecommendViewModel/CocktailRecommendViewModel.swift
@@ -10,27 +10,27 @@ import Combine
 
 protocol CocktailRecommendViewModel {
     var briefDescriptionListPublisher: Published<[BriefDescription]>.Publisher { get }
-
+    
     func fetchBriefDescription()
 }
 
 class DefaultCocktailRecommendViewModel: CocktailRecommendViewModel {
+    private var cancelBag: Set<AnyCancellable> = []
+    private let fetchBriefDescriptionUseCase: FetchBriefDescriptionUsecase
     @Published var briefDescriptionList: [BriefDescription] = []
     
     var briefDescriptionListPublisher: Published<[BriefDescription]>.Publisher { $briefDescriptionList }
     
-    private var cancelBag: Set<AnyCancellable> = []
-    
-    private let fetchBriefDescriptionUseCase: FetchBriefDescriptionUsecase
-    
     init(fetchBriefDescriptionUseCase: FetchBriefDescriptionUsecase) {
         self.fetchBriefDescriptionUseCase = fetchBriefDescriptionUseCase
     }
-    
+}
+
+//MARK: - FetchBriefDescription
+extension DefaultCocktailRecommendViewModel {
     func fetchBriefDescription() {
         fetchBriefDescriptionUseCase.execute().sink(receiveCompletion: { print("\($0)")}, receiveValue: {
             self.briefDescriptionList = $0.briefDescriptionList
-            
         }).store(in: &cancelBag)
     }
 }
