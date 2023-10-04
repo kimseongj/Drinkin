5b4d70c99e4debe61fc153c63f7ac2cfcf8ac6ff
kimseongj <ksj8277@naver.com>
2023-10-04 23:21:39 +0900

    refactor: AppDIContainer의 tokenManager, provider init

--- Drinkin/Application/DIContainer/AppDIContainer.swift
+++ Drinkin/Application/DIContainer/AppDIContainer.swift
@@ -8,42 +8,58 @@
 import Foundation
 
 final class AppDIContainer {
-    let keyChainManager = KeychainManager()
-    let provider = Provider()
+
+
+    private let tokenManager: TokenManager
+    private let provider: Provider
+    
+    init(tokenManager: TokenManager, provider: Provider) {
+        self.tokenManager = tokenManager
+        self.provider = provider
+    }
     
     func makeTriedCocktailSelectionDIContainer() -> TriedCocktailSelectionDIContainer {
-        return TriedCocktailSelectionDIContainer()
+        return TriedCocktailSelectionDIContainer(tokenManager: tokenManager,
+                                                 provider: provider)
     }
     
     func makeCocktailRecommendDIContainer() -> CocktailRecommendDIContainer {
-        return CocktailRecommendDIContainer()
+        return CocktailRecommendDIContainer(tokenManager: tokenManager,
+                                            provider: provider)
     }
     
     func makeProductDetailDIContainer() -> ProductDetailDIContainer {
-        return ProductDetailDIContainer()
+        return ProductDetailDIContainer(tokenManager: tokenManager,
+                                        provider: provider)
     }
     
     func makeCocktailFilterDICotainer() -> CocktailFilterDICotainer {
-        return CocktailFilterDICotainer()
+        return CocktailFilterDICotainer(tokenManager: tokenManager,
+                                        provider: provider)
     }
     
     func makeMyHomeBarDIContainer() -> MyHomeBarDIContainer {
-        return MyHomeBarDIContainer()
+        return MyHomeBarDIContainer(tokenManager: tokenManager,
+                                    provider: provider)
     }
     
     func makeLoginSettingDIContainer() -> LoginSettingDIContainer {
-        return LoginSettingDIContainer()
+        return LoginSettingDIContainer(tokenManager: tokenManager,
+                                       provider: provider)
     }
     
     func makeAddIngredientDIContainer() -> AddIngredientDIContainer {
-        return AddIngredientDIContainer()
+        return AddIngredientDIContainer(tokenManager: tokenManager,
+                                        provider: provider)
     }
     
     func makeSavedCocktailListDIContainer() -> SavedCocktailListDIContainer {
-        return SavedCocktailListDIContainer()
+        return SavedCocktailListDIContainer(tokenManager: tokenManager,
+                                            provider: provider)
     }
     
     func makeUserMadeCocktailListDIContainer() -> UserMadeCocktailListDIContainer {
-        return UserMadeCocktailListDIContainer()
+        return UserMadeCocktailListDIContainer(tokenManager: tokenManager,
+                                               provider: provider)
     }
 }
