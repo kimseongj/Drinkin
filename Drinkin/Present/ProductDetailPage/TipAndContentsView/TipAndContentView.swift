006591956d931106fe715484966044ec1050a587
kimseongj <ksj8277@naver.com>
2023-10-06 23:09:45 +0900

    feat: TipAndContentView 구현

---
+++ Drinkin/Present/ProductDetailPage/TipAndContentsView/TipAndContentView.swift
@@ -0,0 +1,25 @@
+//
+//  TipAndContentView.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/10/06.
+//
+
+import UIKit
+import SnapKit
+
+final class TipAndContentView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+
+
