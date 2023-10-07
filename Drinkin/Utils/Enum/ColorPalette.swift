b004fdd6b2fc2113181b0ad71cadd0dfe944a0bb
kimseongj <ksj8277@naver.com>
2023-10-07 20:12:52 +0900

    feat: ColorPalette에 subTitleGrayColor 구현

--- Drinkin/Utils/Enum/ColorPalette.swift
+++ Drinkin/Utils/Enum/ColorPalette.swift
@@ -11,4 +11,6 @@ enum ColorPalette {
     static let themeColor: UIColor = UIColor(red: 1, green: 0.706, blue: 0.259, alpha: 1)
     
     static let categoryCellColor: UIColor = UIColor(red: 0.946, green: 0.946, blue: 0.946, alpha: 1)
+    
+    static let subTitleGrayColor: UIColor = UIColor(red: 0.472, green: 0.465, blue: 0.453, alpha: 1)
 }
