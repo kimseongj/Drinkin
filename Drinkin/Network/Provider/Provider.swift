357b46e3b3ac8b7b243640be2b5609d6121b8f02
kimseongj <ksj8277@naver.com>
2023-10-16 11:18:33 +0900

    feat: Provider에 postData 구현

--- Drinkin/Network/Provider/Provider.swift
+++ Drinkin/Network/Provider/Provider.swift
@@ -9,9 +9,18 @@ import Foundation
 import Combine
 
 struct Provider {
-    func fetchData<T: Decodable>(endpoint: EndpointMakeable)  -> AnyPublisher<T, Error> {
+    func fetchData<T: Decodable>(endpoint: EndpointMakeable) -> AnyPublisher<T, Error> {
         let request = endpoint.makeURLRequest()
-
+        
+        return URLSession.shared.dataTaskPublisher(for: request!)
+            .map { $0.data }
+            .decode(type: T.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
+    }
+    
+    func postData<B: Encodable, T: Decodable>(endpoint: EndpointMakeable, bodyItem: B) -> AnyPublisher<T, Error> {
+        let request = endpoint.makeJsonPostRequest(endPoint: endpoint, bodyItem: bodyItem)
+        
         return URLSession.shared.dataTaskPublisher(for: request!)
             .map { $0.data }
             .decode(type: T.self, decoder: JSONDecoder())
