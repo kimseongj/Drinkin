fb2918a75b48e92229963898b1f680c0a8d7a13f
kimseongj <ksj8277@naver.com>
2023-10-18 16:42:20 +0900

    feat: BaseBrandInformationEndpoint 구현

---
+++ Drinkin/Network/Endpoint/UIEndpoint/BaseBrandInformationEndpoint.swift
@@ -0,0 +1,20 @@
+//
+//  BaseBrandInformationEndpoint.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/10/18.
+//
+
+import Foundation
+
+struct BaseBrandInformationEndpoint: EndpointMakeable {
+    var baseURL: String = "https://41b9e562-5f0b-4f0c-98ab-3e657e0e7dea.mock.pstmn.io"
+    
+    var path: String = "/brand"
+    
+    var method: String = HTTPMethod.get.rawValue
+    
+    var queryItems: [URLQueryItem] = []
+    
+    var header: [String : String]?
+}
