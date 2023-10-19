66ecd9ae213618b37abd077145b95e0668448ac2
kimseongj <ksj8277@naver.com>
2023-10-19 14:16:34 +0900

    feat: BaseBrandInformationViewModel 구현

---
+++ Drinkin/Present/BaseBrandInformation/BaseBrandInformationViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  BaseBrandInformationViewModel.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/10/18.
+//
+
+import Foundation
+import Combine
+
+protocol BaseBrandInformationViewModel {
+    var baseBrandDetailPublisher: Published<BaseBrandDetail?>.Publisher { get }
+    
+    func fetchBaseBrandDetail()
+}
+
+final class DefaultBaseBrandInformationViewModel: BaseBrandInformationViewModel {
+    private let baseBrandDetailRepository: BaseBrandDetailRepository
+    private var cancelBag: Set<AnyCancellable> = []
+    
+    @Published var baseBrandDetail: BaseBrandDetail?
+    var baseBrandDetailPublisher: Published<BaseBrandDetail?>.Publisher { $baseBrandDetail }
+    
+    init(baseBrandDetailRepository: BaseBrandDetailRepository) {
+        self.baseBrandDetailRepository = baseBrandDetailRepository
+    }
+    
+    func fetchBaseBrandDetail() {
+        baseBrandDetailRepository.fetchBaseBrandDetail().sink(receiveCompletion: { print("\($0)")}, receiveValue: {
+            self.baseBrandDetail = $0
+        }).store(in: &cancelBag)
+    }
+}
+
