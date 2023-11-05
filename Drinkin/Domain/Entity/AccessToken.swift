5fafc26cdfa95a493d5c7c3a869c9e2263456c9f
kimseongj <ksj8277@naver.com>
2023-11-05 10:40:10 +0900

    refactor: AccessToken Entity 구현

---
+++ Drinkin/Domain/Entity/AccessToken.swift
@@ -0,0 +1,16 @@
+//
+//  AccessToken.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/11/03.
+//
+
+import Foundation
+
+struct AccessToken: Decodable {
+    let accessToken: String
+    
+    private enum CodingKeys: String, CodingKey {
+        case accessToken = "access_token"
+    }
+}
