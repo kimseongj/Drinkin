6621c747b3a43d30c9256dcd2b761f39b253f5ee
kimseongj <ksj8277@naver.com>
2023-11-10 20:43:52 +0900

    refactor: CocktailIDList, PostResponse Entity 구현

---
+++ Drinkin/Domain/Entity/Cocktail/CocktailIDList.swift
@@ -0,0 +1,12 @@
+//
+//  CocktailIDList.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/11/09.
+//
+
+import Foundation
+
+struct CocktailIDList: Encodable {
+    let cocktailIDList: [Int]
+}
