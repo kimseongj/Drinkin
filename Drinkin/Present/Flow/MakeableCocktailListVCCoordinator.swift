1f8d57ea90537583c3ac73464af66186c0742f16
kimseongj <ksj8277@naver.com>
2023-11-12 21:57:34 +0900

    feat: MakeableCocktailListVCCoordinator 구현

---
+++ Drinkin/Present/Flow/MakeableCocktailListVCCoordinator.swift
@@ -0,0 +1,40 @@
+//
+//  MakeableCocktailListVCCoordinator.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/11/12.
+//
+
+import UIKit
+
+protocol MakeableCocktailListVCFlow: AnyObject {
+    func pushProductDetailVC(cocktailID: Int)
+}
+
+final class MakeableCocktailListVCCoordinator: Coordinator, MakeableCocktailListVCFlow {
+    var navigationController: UINavigationController
+    let appDIContainer: AppDIContainer
+    let baseBrandID: Int
+    
+    init(navigationController: UINavigationController, appDIContainer: AppDIContainer, baseBrandID: Int) {
+        self.navigationController = navigationController
+        self.appDIContainer = appDIContainer
+        self.baseBrandID = baseBrandID
+    }
+    
+    func start() {
+        let makeableCocktailListDIContainer = appDIContainer.makeMakeableCocktailListDIContainer(brandID: baseBrandID)
+        
+        let vc = makeableCocktailListDIContainer.makeMakeableCocktailListViewController()
+        vc.delegate = self
+        
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+    func pushProductDetailVC(cocktailID: Int) {
+        let productDetailVCCoordinator = ProductDetailVCCoordinator(navigationController: navigationController,
+                                                                    appDIContainer: appDIContainer,
+                                                                    cocktailID: cocktailID)
+        productDetailVCCoordinator.start()
+    }
+}
