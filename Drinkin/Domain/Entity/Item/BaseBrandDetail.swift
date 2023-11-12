e9291822abd61f88421c80c0d74eda1868cc8ba3
kimseongj <ksj8277@naver.com>
2023-11-12 15:11:45 +0900

    refactor: BaseBrandDetail entity에 id 구현

--- Drinkin/Domain/Entity/Item/BaseBrandDetail.swift
+++ Drinkin/Domain/Entity/Item/BaseBrandDetail.swift
@@ -8,12 +8,14 @@
 import Foundation
 
 struct BaseBrandDetail: Codable, Hashable {
+    let id: Int
     let imageURI: String
     let baseBrandName: String
 //    let classification: String
     let abv: String
     
     enum CodingKeys: String, CodingKey {
+        case id
         case imageURI = "image_uri"
         case baseBrandName = "base_brand_name_ko"
 //        case classification
