1afec4deb302714f3cb05787ffbc10fa7ea03391
kimseongj <ksj8277@naver.com>
2023-11-13 13:29:08 +0900

    feat: MakeableCocktailListViewController binding 구현

--- Drinkin/Present/MakeableCocktailList/ViewModel/MakeableCocktailListViewModel.swift
+++ Drinkin/Present/MakeableCocktailList/ViewModel/MakeableCocktailListViewModel.swift
@@ -9,17 +9,28 @@ import Foundation
 import Combine
 
 protocol MakeableCocktailListViewModel {
+    var makeableCocktailList: [MakeableCocktail] { get }
+    var makeableCocktailListPublisher: Published<[MakeableCocktail]>.Publisher { get }
     
+    func fetchMakeableCocktailList()
 }
 
 final class DefaultMakeableCocktailListViewModel: MakeableCocktailListViewModel {
     private let makeableCocktailListRepository: MakeableCocktailListRepository
+    private var cancelBag: Set<AnyCancellable> = []
+    
+    @Published var makeableCocktailList: [MakeableCocktail] = []
+    var makeableCocktailListPublisher: Published<[MakeableCocktail]>.Publisher { $makeableCocktailList }
     
     init(makeableCocktailListRepository: MakeableCocktailListRepository) {
         self.makeableCocktailListRepository = makeableCocktailListRepository
     }
     
     func fetchMakeableCocktailList() {
-        
+        makeableCocktailListRepository.fetchMakeableCocktails().receive(on: RunLoop.main).sink(receiveCompletion: { print("\($0)")}, receiveValue: { [weak self] in
+            guard let self = self else { return }
+            
+            self.makeableCocktailList = $0.makeableCocktailList
+        }).store(in: &cancelBag)
     }
 }
