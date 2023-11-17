c05c98df3af180246d0c44278c1bdbe511c12d18
kimseongj <ksj8277@naver.com>
2023-11-17 14:21:52 +0900

    refactor: LoginProvider에 postAccessToken 재구현 및 renewAccessTokenPublisher 삭제

--- Drinkin/Network/Provider/LoginProvider.swift
+++ Drinkin/Network/Provider/LoginProvider.swift
@@ -8,47 +8,25 @@
 import Foundation
 import Combine
 
-class LoginProvider {
-    private let keychainManager = TokenManager()
-    private var cancelBag: Set<AnyCancellable> = []
+protocol LoginProvider {
+    func postAccessToken(endpoint: EndpointMakeable, accessToken: String) -> AnyPublisher<LoginToken, Error>
+}
 
-    func makePostRequest(endpoint: EndpointMakeable, accessToken: String, holdedCocktailList: [Int]) -> URLRequest? {
-        guard var request = endpoint.makeURLRequest() else { return nil }
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let requestBody = FirstLogin(accessToken: accessToken, holdedItemList: holdedCocktailList)
-        
-        do {
-            request.httpBody = try JSONEncoder().encode(requestBody)
-        } catch {
-            print("PostError")
-        }
-        
-        return request
+struct DefaultLoginProvider: LoginProvider {
+    private let tokenManager: TokenManager
+
+    init(tokenManager: TokenManager) {
+        self.tokenManager = tokenManager
     }
     
-    func postAccessTokenAndHoldedItem(request: URLRequest?, completion: @escaping () -> Void) {
-        guard let request else { return }
+    func postAccessToken(endpoint: EndpointMakeable, accessToken: String) -> AnyPublisher<LoginToken, Error> {
+        let encodableAccessToken = AccessToken(accessToken: accessToken)
         
-        URLSession.shared.dataTaskPublisher(for: request)
-            .map(\.data).decode(type: FirstLoginResponse.self, decoder: JSONDecoder())
-            .sink(receiveCompletion: { completion in
-                switch completion {
-                case .finished:
-                    break
-                case .failure(let error):
-                    print("Request failed with error: \(error)")
-                }
-            }, receiveValue: { [weak self] in
-                guard let self = self else { return }
-                
-                do {
-                    try self.keychainManager.saveToken(tokenType: TokenType.accessToken,
-                                                       token: $0.accessToken)
-                    completion()
-                } catch {
-                    print("saveError")
-                }
-            }).store(in: &cancelBag)
+        let request = endpoint.makeJsonPostRequest(bodyItem: encodableAccessToken)
+        
+        return URLSession.shared.dataTaskPublisher(for: request!)
+            .map { $0.data }
+            .decode(type: LoginToken.self, decoder: JSONDecoder())
+            .eraseToAnyPublisher()
     }
 }
