1e549c1d628278577ac5a52bf1f1e3c79d2d9424
kimseongj <ksj8277@naver.com>
2023-11-17 16:08:17 +0900

    refactor: ProductDetailViewController, ProductDetailViewModel error처리 구현

--- Drinkin/Present/ProductDetailPage/ViewModel/ProductDetailViewModel.swift
+++ Drinkin/Present/ProductDetailPage/ViewModel/ProductDetailViewModel.swift
@@ -14,6 +14,7 @@ protocol ProductDetailViewModelInput {
 }
 
 protocol ProductDetailViewModelOutput {
+    var errorHandlingPublisher: Published<APIError>.Publisher { get }
     var cocktailDescriptionPublisher: Published< CocktailDescription?>.Publisher { get }
 }
 
@@ -23,6 +24,7 @@ final class DefaultProductDetailViewModel: ProductDetailViewModel {
     private let cocktailDetailRepository: CocktailDetailRepository
     private var cancelBag: Set<AnyCancellable> = []
     
+    @Published var errorType: APIError = APIError.noError
     @Published var cocktailDescription: CocktailDescription?
     
     //MARK: - Init
@@ -31,15 +33,40 @@ final class DefaultProductDetailViewModel: ProductDetailViewModel {
     }
     
     //MARK: - Output
+    var errorHandlingPublisher: Published<APIError>.Publisher { $errorType }
     var cocktailDescriptionPublisher: Published<CocktailDescription?>.Publisher { $cocktailDescription }
     
     //MARK: - Input
     func fetchDescription() {
         cocktailDetailRepository.fetchCocktailDescription()
-            .sink(receiveCompletion: { print("\($0)")}, receiveValue: { [weak self] in
-                guard let self = self else { return }
-                
-                self.cocktailDescription = $0
-            }).store(in: &cancelBag)
+            .sink(
+                receiveCompletion: { [weak self] completion in
+                    guard let self = self else { return }
+                    switch completion {
+                    case .failure(let error):
+                        switch error {
+                        case .unauthorized:
+                            self.errorType = .unauthorized
+                        case .notFound:
+                            self.errorType = .notFound
+                        case .networkError(_):
+                            self.errorType = .networkError(error)
+                        case .decodingError:
+                            self.errorType = .decodingError
+                        case .refreshTokenExpired:
+                            self.errorType = .refreshTokenExpired
+                        case .noError:
+                            break
+                        }
+                    case .finished:
+                        return
+                    }
+                },
+                receiveValue: { [weak self] in
+                    guard let self = self else { return }
+                    
+                    self.cocktailDescription = $0
+                }
+            ).store(in: &cancelBag)
     }
 }
