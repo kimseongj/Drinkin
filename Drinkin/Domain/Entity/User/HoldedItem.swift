ead31d22b88d76e16b99d49f1ee7efce26de2502
kimseongj <ksj8277@naver.com>
2023-12-08 13:27:19 +0900

    refactor: HoldedItem의 Codable을 Decodable로 수정

--- Drinkin/Domain/Entity/User/HoldedItem.swift
+++ Drinkin/Domain/Entity/User/HoldedItem.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct HoldedItem: Codable, Hashable {
+struct HoldedItem: Decodable, Hashable {
     let holdedItemList: [String]
     
     enum CodingKeys: String, CodingKey {
