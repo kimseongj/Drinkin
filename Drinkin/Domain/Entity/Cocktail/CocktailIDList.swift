ef86daf49d8fc9e3a748ad758468538422d16dfb
kimseongj <ksj8277@naver.com>
2023-12-11 13:13:05 +0900

    refactor: CocktailIDList entity json 데이터 이름 수정

--- Drinkin/Domain/Entity/Cocktail/CocktailIDList.swift
+++ Drinkin/Domain/Entity/Cocktail/CocktailIDList.swift
@@ -9,4 +9,8 @@ import Foundation
 
 struct CocktailIDList: Encodable {
     let cocktailIDList: [Int]
+    
+    enum CodingKeys: String, CodingKey {
+        case cocktailIDList = "cocktail_id_list"
+    }
 }
