af492625f72d888cffc03e61686213e98852c0e5
kimseongj <ksj8277@naver.com>
2023-12-13 14:38:30 +0900

    refactor: LoginManager를 AuthenticationManager로 이름 변경 및 login, logout, accessTokenStatusPublisher메서드 추가

---
+++ Drinkin/Utils/LoginManager/AuthenticationManager.swift
@@ -0,0 +1,60 @@
+//
+//  LoginManager.swift
+//  Drinkin
+//
+//  Created by kimseongjun on 2023/11/21.
+//
+
+import Foundation
+import Combine
+
+protocol AuthenticationManager {
+    func isAuthenticated() -> Bool
+    func login(accessToken: String, refreshToken: String)
+    func logout()
+    func accessTokenStatusPublisher() -> AnyPublisher<Bool, Never>
+}
+
+final class DefaultAuthenticationManager: AuthenticationManager {
+    private let tokenManager: TokenManager
+    
+    private lazy var isAuthenticatedSubject = CurrentValueSubject<Bool, Never>(isAuthenticated())
+    
+    init(tokenManager: TokenManager) {
+        self.tokenManager = tokenManager
+        
+    }
+    
+    func isAuthenticated() -> Bool {
+        do {
+            let accessToken = try tokenManager.readToken(tokenType: TokenType.accessToken)
+            return accessToken != nil
+        } catch {
+            return false
+        }
+    }
+    
+    func login(accessToken: String, refreshToken: String) {
+        do {
+            try tokenManager.saveToken(tokenType: TokenType.accessToken, token: accessToken)
+            try tokenManager.saveToken(tokenType: TokenType.refreshToken, token: refreshToken)
+            isAuthenticatedSubject.send(true)
+        } catch {
+            
+        }
+    }
+    
+    func logout() {
+        do {
+            try tokenManager.deleteToken(tokenType: TokenType.accessToken)
+            try tokenManager.deleteToken(tokenType: TokenType.refreshToken)
+            isAuthenticatedSubject.send(false)
+        } catch {
+            
+        }
+    }
+    
+    func accessTokenStatusPublisher() -> AnyPublisher<Bool, Never> {
+            return isAuthenticatedSubject.eraseToAnyPublisher()
+        }
+}
